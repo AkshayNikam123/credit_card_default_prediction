3b969098d02484c360c90a3c0efcbc3cd26d13d5
Akshay Nikam <nikam.ak152@gmail.com>
2023-07-26 18:15:56 +0530

    changes application.py

--- application.py
+++ application.py
@@ -93,10 +93,10 @@ def start_trainer():
     return render_template('start_trainer.html', logs=log_content)
 
 
-
+"""
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000,workers=4)
-    
-    
+    app.run()
+"""  
+    #host="0.0.0.0",port=8000,workers=4
     
 
